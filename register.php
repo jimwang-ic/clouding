453ef7066040e1a88653c34e13eb5b55a2266cd6
PO-WEI LIANG <liang0816tw@gmail.com>
2011-06-19 16:38:26 +0800

    add register.php

---
+++ register.php
@@ -0,0 +1,10 @@
+﻿<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name="form" method="post" action="register_finish.php">
+<p><strong>請填寫註冊資料</strong></p>
+帳號：<input type="text" name="id" /> <br>
+密碼：<input type="password" name="pw" /> <br>
+再一次輸入密碼：<input type="password" name="pw2" /> <br>
+性別：<input type="text" name="gender" /> <br>
+地區：<input type="text" name="region" /> <br>
+<input type="submit" name="button" value="確定" />
+</form>
