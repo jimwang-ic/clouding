bb8e2105fb8ca3ba4fff7f0d91fdd165625378b0
PO-WEI LIANG <liang0816tw@gmail.com>
2011-06-22 00:11:54 +0800

    update

--- function.php
+++ function.php
@@ -1,11 +1,11 @@
 <?php   
 function ShowAuthor($author){
-		
+	
 		$sql = "SELECT photo_route FROM member where ID = '$author'";
 		$result1 = mysql_query($sql);
 		$tmp = @mysql_fetch_row($result1);
-		$friend[$n]['picture']=$tmp[0];	
-		return $friend[$n]['picture'];
+		
+		return $tmp[0];
 }
 
 
