a6fe3c48ec09e2826a89b9101ebf73332d84bfb1
cbtzeng <cbtzeng@gmail.com>
2011-06-22 01:10:46 +0800

    update

--- WhatTOeatToday.php
+++ WhatTOeatToday.php
@@ -7,19 +7,44 @@ if (!isset($_SESSION)) {
 include("mysql_connect.inc.php");
 
 $id = $_SESSION['username'];
-
 $sql = "SELECT * FROM interest WHERE ID = '$id'";
-
 $result = mysql_query($sql);
-
 $row = mysql_fetch_row($result);
- 
-sort($row);
 
-foreach ($row as $key => $val) {
-    echo "Attributes[" . $key . "] = " . $val . "<br />";
+$n = 1;
+$t_0 = 1;
+$t_1 = 1;
+$t_2 = 1;
+$s = $row['1'];
+while(++$n<11){
+  if($row[$n]>$s){
+     $s = $row[$n];
+	 $t_0 = $n;
+  } 
 }
+$row[$t_0] = 0;
 
+$n = 1;
+$s = $row['1'];
+while(++$n<11){
+  if($row[$n]>$s){
+     $s = $row[$n];
+	 $t_1 = $n;
+  } 
+}
+$row[$t_1] = 0;
 
+$n = 1;
+$s = $row['1'];
+while(++$n<11){
+  if($row[$n]>$s){
+     $s = $row[$n];
+	 $t_2 = $n;
+  }
+}
+$row[$t_2] = 0;
+
+$f_rank = array("$t_0","$t_1","$t_2");
+print_r($f_rank);
 
 ?>
