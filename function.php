cd1e7d1177de5c06ddc2abc7df556a91908bf6df
cbtzeng <cbtzeng@gmail.com>
2011-06-22 00:10:14 +0800

    update

--- function.php
+++ function.php
@@ -4,8 +4,9 @@ function ShowAuthor($author){
 		$sql = "SELECT photo_route FROM member where ID = '$author'";
 		$result1 = mysql_query($sql);
 		$tmp = @mysql_fetch_row($result1);
-		$friend[$n]['picture']=$tmp[0];	
-		return $friend[$n]['picture'];
+		
+		
+		return $tmp[0];
 }
 
 
